f8b308a332b4fe50f397399c7975118171507b02
gcochard <greg.cochard@gmail.com>
2013-11-29 16:11:51 -0800

    Finishing downloading of all photos as zip file.

--- routes/photos.js
+++ routes/photos.js
@@ -1,7 +1,11 @@
 var config = require('../modules/config')
   , app = config.app
+  , async = require('async')
+  , zipper = require('zipper').Zipper
   , models = require('../models')
   , Photo = models.Photo
+  , path = require('path')
+  , fs = require('fs')
   ;
 
 app.get('/photos',function(req,res){
@@ -15,4 +19,38 @@ app.get('/photos',function(req,res){
           thanksdiv:'hidden'
       });
     });
+});
+
+app.get('/photos/all',function(req,res){
+  'use strict';
+  fs.unlink('/tmp/photos.zip',function(error){
+    var photoArray = [],i,ii, zipfile = new zipper('/tmp/photos.zip'),
+    zipme = function(photo){
+      console.log('zipping %s', path.basename(photo.lrgloc));
+      return function(cb){
+        zipfile.addFile(photo.lrgloc,path.basename(photo.lrgloc),function(err){
+          var res = {success:'success'};
+          if(err){
+            console.log(err);
+            res = null;
+          }
+          console.log('done zipping %s', path.basename(photo.lrgloc));
+          cb(err,res);
+        });
+      };
+    };
+    Photo.find({category:'engagement',shown:true}).lean().sort({order:1}).exec(function(err,photos){
+      for(i=0,ii=photos.length;i<ii;i++){
+        photoArray.push(zipme(photos[i]));
+      }
+      async.series(photoArray,function(err,results){
+        console.log('done, ',results);
+        if(!err){
+          res.download('/tmp/photos.zip');
+        } else {
+          res.send(500,{error:err});
+        }
+      });
+    });
+  });
 });
